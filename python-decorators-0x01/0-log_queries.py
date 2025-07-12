7558fee727e84af0b0cd5a00730c0782fea1ce20
Francis Tate Chekure <ftchekure@gmail.com>
2025-07-12 21:37:28 +0200

    Add timestamp to SQL query logs

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,12 +1,13 @@
 import sqlite3
 import functools
+from datetime import datetime
 
-#### decorator to lof SQL queries
+#### decorator to log SQL queries
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         query = args[0] if args else kwargs.get('query', '')
-        print(f"Executing SQL Query: {query}")
+        print(f"Executing SQL Query: {query} at {datetime.now()}")
         return func(*args, **kwargs)
     return wrapper
 
